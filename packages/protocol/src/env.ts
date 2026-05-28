3fc30740e2433cd95d98d839b3cb3410a50a726d
David Wolf <david.wolf@chax.at>
2026-05-28 12:51:22 +0200

    protocol: env + interval constants

---
+++ packages/protocol/src/env.ts
@@ -0,0 +1,8 @@
+export const AGMUX_SESSION_ID_ENV = "AGMUX_SESSION_ID";
+export const AGMUX_HUB_URL_ENV = "AGMUX_HUB_URL";
+
+export const AGMUX_STATE_DIR_DEFAULT = ".agmux";
+export const AGMUX_CONFIG_SUBPATH = ".config/agmux/config.toml";
+
+export const HEARTBEAT_INTERVAL_MS = 30_000;
+export const LOST_THRESHOLD_MS = 60_000;
